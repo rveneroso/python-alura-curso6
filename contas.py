54fd8741908d73433a3de860402ee991614f5311
Renato Veneroso <rveneroso@gmail.com>
2022-01-31 10:39:12 -0300

    Alteração da classe Conta tornando-a abstrata e definindo o método passa_o_mes() como sendo abstrato. Criação da subclasse ContaInvestimento que não implementa o método passa_o_mes() e, consequentemente, não pode ser instanciada.

--- contas.py
+++ contas.py
@@ -1,4 +1,10 @@
-class Conta:
+#
+# Torna a classe Conta abstrata. Embora ela possua alguns métodos implementados, o fato dela
+# possuir um único método abstrato a torna automaticamente uma classe abstrata.
+#
+#
+from abc import ABCMeta, abstractmethod
+class Conta(metaclass=ABCMeta):
     def __init__(self, codigo):
         self._codigo = codigo
         self._saldo = 0
@@ -8,6 +14,12 @@ class Conta:
 
     def __str__(self):
         return "[>>> Código {} Saldo {} <<<]".format(self._codigo, self._saldo)
+
+    #
+    # Define o método passa_o_mes abstrato.
+    @abstractmethod
+    def passa_o_mes(self):
+        pass
 
 class ContaCorrente(Conta):
 
@@ -19,6 +31,9 @@ class ContaPoupanca(Conta):
     def passa_o_mes(self):
         self._saldo *= 1.01
         self._saldo -= 3
+
+class ContaInvestimento(Conta):
+    pass
 
 #
 # Cria uma conta do tipo ContaCorrente
@@ -49,3 +64,10 @@ contas = [cc, cp] # A lista contém objetos do tipo Conta
 for conta in contas:
     conta.passa_o_mes()
     print(conta) # O resultado será o mesmo obtido antes de se usar lista
+
+#
+# A tentativa de criar uma instância de ContaInvestimento resultará em erro uma vez que essa classe
+# não implementa o método abstrato passa_o_mes definido na classe Conta:
+# TypeError: Can't instantiate abstract class ContaInvestimento with abstract method passa_o_mes
+#
+ci = ContaInvestimento(18)
